d0a3c66c5137d3e95f5382daaf2e011cdd950af4
Duders591 <dasjgkdsfhdsjkfadshlk@gmail.com>
2025-12-22 23:02:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+console.log("JavaScript linked successfully!");
+let sliderAmount = parseInt(document.getElementById("slider").value);
+const counter = document.getElementById("testCounter");
+const berekeningen = document.getElementById("berekeningen").children;
+let plusElkaar = [];
+let i = 0;
+let e = 0;
+let antwoord = 0;
+function update() {
+    sliderAmount = parseInt(document.getElementById("slider").value);
+    counter.textContent = sliderAmount;
+}
+update()
+document.getElementById("slider").oninput = update;
+function bereken() {
+    console.log("Calculation started...")
+    update();
+    while (i < sliderAmount) {
+        plusElkaar.push(berekeningen[i].value);
+        i++
+        if (i >= sliderAmount) {
+            while (e < sliderAmount) {
+                antwoord += parseInt(plusElkaar[e]);
+                e++
+                if (e >= sliderAmount) {
+                    antwoord /= sliderAmount;
+                    document.getElementById("antwoord").textContent = `Het gemiddelde van die ${sliderAmount} cijfers is: ${antwoord}`;
+                    console.log(antwoord);
+                }
+            }
+        }
+    }
+    i = 0;
+    e = 0;
+    antwoord = 0;
+    plusElkaar = []
+}
